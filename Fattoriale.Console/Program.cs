578d5ae539e2a81d969782a82474270e6ac8cd75
Nicolò Carandini <n.carandini@outlook.com>
2018-02-14 11:05:30 +0100

    First commit

---
+++ Fattoriale.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Fattoriale.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
