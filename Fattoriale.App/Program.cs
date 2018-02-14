e78b2f0f2b275dc2757c52fa52c4b1814610be51
pc2 <pc2@PC>
2018-02-14 12:12:05 +0100

    programma luca

--- Fattoriale.App/Program.cs
+++ Fattoriale.App/Program.cs
@@ -23,7 +23,14 @@ namespace Fattoriale.App
 
         static int Fattoriale(int n)
         {
-            return 0;
+            if(n<2)
+            {
+                return 1;
+            }
+            else
+            {
+                return n*Fattoriale(n-1);
+            }
         }
     }
 }
