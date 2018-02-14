56f5fc5cecf5df2c14cace20b9f99d729eb051ce
rossanacaridi91 <ro>
2018-02-14 12:03:02 +0100

    modifica fattoriale

--- Fattoriale.App/Program.cs
+++ Fattoriale.App/Program.cs
@@ -23,7 +23,13 @@ namespace Fattoriale.App
 
         static int Fattoriale(int n)
         {
-            return 0;
+            if (n < 2)
+            {
+                return 1;
+            }
+
+            return n * Fattoriale(n - 1);
+
         }
     }
 }
